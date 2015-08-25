07786f958dd99b4a32ee71f49dea90a8408fa2f4
rensramos <rensramos17@gmail.com>
2015-08-25 14:57:51 +0800

    update mouse-scroll function

--- js/home.js
+++ js/home.js
@@ -4,17 +4,12 @@ phHome.phHomeFunction = function() {
 };
 
 phHome.pageScroll = function(){
-    $('.page-scroll a').on('click', function(event) {
-
-        var target = $( $(this).attr('href') );
-
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top - 75
-            }, 1000);
-        }
-
+    $('.page-scroll a').on('click', function(e){
+        e.preventDefault();
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 75
+        }, 1500, 'easeInOutExpo');
     });
 };
 
