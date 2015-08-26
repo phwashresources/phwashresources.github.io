d5b3b95e01f7391dba0447246c54a25af5239792
rensramos <rensramos17@gmail.com>
2015-08-26 16:39:54 +0800

    add newsfeed slider

---
+++ js/phats.js
@@ -0,0 +1,43 @@
+var phPhats = phPhats || {};
+phPhats.phPhatsFunction = function() {
+    phPhats.productImageSlider();
+};
+
+phPhats.productImageSlider = function(){
+    var autoPlaySlider = $('#product-img-slider').lightSlider({
+        item: 5,
+        controls: false,
+        speed: 2000,
+        pause: 4000,
+        auto: true,
+        loop:true,
+        enableDrag:false,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:4,
+                    enableDrag:true
+                }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+
+                }
+            }
+        ]
+    });
+    $('#product-img-slider').on('mouseenter',function(){
+        autoPlaySlider.pause();
+    });
+    $('#product-img-slider').on('mouseleave',function(){
+        autoPlaySlider.play();
+    });
+};
+
+
+$(function(){
+    phPhats.phPhatsFunction();
+})
