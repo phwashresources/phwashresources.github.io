fbe2edb3907754e0c4e84da6e4fd98b0c0a1711f
rensramos <rensramos17@gmail.com>
2015-10-12 13:05:57 +0800

    Remove videoButton function

--- js/home.js
+++ js/home.js
@@ -2,18 +2,6 @@ var phHome = phHome || {};
 phHome.phHomeFunction = function() {
     phHome.pageScroll();
     phHome.navbarScroll();
-    phHome.videoButton();
-};
-
-phHome.videoButton = function(){
-    var videoButton = $('#video-button');
-    var videoContainer = $('#video-section .video-container');
-    var videoMenuContainer = $('#video-section .video-container #video-menu-container');
-    videoButton.on('click', function(){
-        videoContainer.addClass('video-on-show');
-        videoMenuContainer.css('display','none');
-        videoContainer.append('<iframe src="https://player.vimeo.com/video/141763062?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true"></iframe>');
-    });
 };
 
 phHome.pageScroll = function(){
